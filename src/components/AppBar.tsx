8355fef71f28deb03f7675aad309c39c6d5b2e87
0x1zen <rajdubalwork@gmail.com>
2024-08-13 11:07:45 +0530

    Built Appbar Component and instlled tailwind

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,16 @@
+function AppBar() {
+    return (
+      <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+        <div className="">
+          <img
+            src="https://i.ibb.co/LY0wYMZ/Logo-12.png"
+            alt="logo"
+            className=" h-10"
+          ></img>
+        </div>
+      </div>
+    );
+  }
+  
+  export default AppBar;
+  
