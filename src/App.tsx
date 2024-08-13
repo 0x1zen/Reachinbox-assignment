65415e1064b63338bc00efff988ff445e127520c
0x1zen <rajdubalwork@gmail.com>
2024-08-13 19:14:59 +0530

    Completed with OneBox.tsx and Added route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import OneBox from "./pages/OneBox"
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div>
+      <BrowserRouter>
+      
+        <Routes>
+        <Route path="/" element={<OneBox/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
