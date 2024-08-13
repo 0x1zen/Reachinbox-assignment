57a6f1feb52a8c54ccf096e3932d50b375c19a10
0x1zen <rajdubalwork@gmail.com>
2024-08-13 11:26:13 +0530

    Built Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/login"
+
 function App() {
 
 
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
